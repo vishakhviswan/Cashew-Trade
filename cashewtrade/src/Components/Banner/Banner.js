d4100759bc6d8adb017e390654e797fd263cbdc9
vishakhviswan <vishakhviswan1995@gmail.com>
2021-06-16 15:05:58 +0530

    second commit

---
+++ cashewtrade/src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Banner
+
---
+++ cashewtrade/src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../Components/Header/Header';
+
+import './Home.css';
+
+
+function Home(props) {
+  return (
+    
+    <div className="homeParentDiv">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default Home;
+ 
